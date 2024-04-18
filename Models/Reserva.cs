cd80658d99bdd07bc33705b21d6c713697440775
Alex <61164399+AlexGabrielSilveira@users.noreply.github.com>
2024-04-18 09:13:46 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,52 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade de hospedes não bateu !");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                var desconto =  Suite.ValorDiaria * (10/100);
+                valor = Suite.ValorDiaria - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
